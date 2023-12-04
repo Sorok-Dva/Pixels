257e4e9da896d3afbc58f567471b8874a9715d5c
Сорок два <sorokdva.developer@gmail.com>
2023-12-04 21:02:27 +0100

    feat: admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Models = require('../orm/models/index')
+const imageHelper = require('../helpers/image')
 
-/* GET admin index. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET admin image */
+router.get('/images/:id', async (req, res, next) => {
+  await Models.Images.findOne({
+    fields: ['data64', 'answer'],
+    where: {
+      id: req.params.id,
+    }
+  }).then((image) => {
+    res.render(`admin/images/view`, { image });
+  }).catch(error => {
+    console.log('Fail to retrieve image from database.', error)
+  })
+})
+
+/* GET admin images add. */
+router.get('/images/add', (req, res, next) => {
+  res.render('admin/images/add');
 });
+
+/* POST admin add image */
+router.post('/images/add', (req, res, next) => {
+  const imageUrl = req.body.url;
+  imageHelper.imageUrlToBase64(imageUrl)
+    .then(async base64Data => {
+      await Models.Images.create({
+        data64: base64Data,
+        answer: req.body.answer,
+        validated: true,
+      }).then((image) => {
+        res.redirect(`/admin/images/${image.id}`);
+      }).catch(error => {
+        console.log('Fail to add image into database.', error)
+      })
+    })
+    .catch(error => {
+      console.error('An error has occurred:', error);
+    });
+})
 
 module.exports = router;
