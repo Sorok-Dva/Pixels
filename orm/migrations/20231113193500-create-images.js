1729907de87ce2566300b5fb67d35d35a9021984
Сорок два <sorokdva.developer@gmail.com>
2023-12-04 20:57:03 +0100

    fix(orm): change image data64 to LONGTEXT

--- orm/migrations/20231113193500-create-images.js
+++ orm/migrations/20231113193500-create-images.js
@@ -10,7 +10,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       data64: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT('long')
       },
       answer: {
         type: Sequelize.STRING
