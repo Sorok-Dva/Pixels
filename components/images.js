0999295f505a1b8e9808a9b12d7a5941590c5b69
Сорок два <sorokdva.developer@gmail.com>
2023-12-08 20:11:48 +0100

    fix: add op.notIn excludesIds once there were played

--- components/images.js
+++ components/images.js
@@ -1,10 +1,22 @@
 const Sequelize = require('sequelize')
+const { Op } = Sequelize;
 const Models = require(`../orm/models/index`);
+
+const excludedIds = [];
 
 const images = {
   retrieveImage: async () => {
     console.log('new image requested')
-    return await Models.Images.findAll({ order: Sequelize.literal('rand()'), limit: 1 }).then((image) => {
+    return await Models.Images.findAll({
+      order: Sequelize.literal('rand()'),
+      limit: 1,
+      where: {
+        id: {
+          [Op.notIn]: excludedIds,
+        },
+      }
+    }).then((image) => {
+      excludedIds.push(image[0].id)
       console.log('the image is : ', image[0].answer)
       return {
         data64: image[0].data64,
