921c78216ced42d0bfcedd0e3a9f346b9a267590
Сорок два <sorokdva.developer@gmail.com>
2023-12-11 19:22:30 +0100

    feat: disable dev console

--- public/javascripts/pixels.js
+++ public/javascripts/pixels.js
@@ -141,3 +141,21 @@ const scrollToBottom = () => {
     scrollTop: $('#answers')[0].scrollHeight
   }, 'fast');
 }
+
+document.onkeydown = (e) => {
+  if (e.key === 123) {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key === 'I') {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key === 'C') {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key === 'J') {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.key === 'U') {
+    e.preventDefault();
+  }
+};
